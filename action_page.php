1e19899eb51c068d6acf10a705c6ad248ba1783e
Krishna Kashiv <47171485+krishnakashiv@users.noreply.github.com>
2019-04-17 12:15:25 +0530

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,19 @@
+<?php
+/*$username = $_POST['uname'];
+$passwordwithout=$_POST['psw'];
+$passhash=sha1( $passwordwithout ), "\n";*/
+$i=0;
+$file=fopen('pwd1.txt', 'r');
+$file1='./hashes1.txt';
+while(! feof($file))
+	{
+		$word=fgets($file). "<br />";
+		$i=$i+1;
+		echo $i."<br>";
+		$hashcompare=sha1 ($word) ;
+		file_put_contents($file1, $hashcompare."\r\n", FILE_APPEND);
+	}
+
+//fclose($file1);
+fclose($file);
+?>
