af5c5077b4218c7f0ed762e286070aaa094a775b
Akshayjain-2812 <aj2812@gmail.com>
2019-04-20 12:44:46 +0530

    db checking

--- action_page.php
+++ action_page.php
@@ -3,6 +3,23 @@ $username = $_POST['uname'];
 $passwordwithout=$_POST['psw'];
 $passhash=sha1( $passwordwithout ), "\n";
 $i=0;
+include 'db_connect.php';
+$id_exists = false;
+$username = mysql_real_escape_string($_GET['id']);
+$sql = "SELECT id FROM members WHERE id='$username'";
+$result = mysql_query($sql);
+
+if ($result == $username)
+{
+        $id_exists = true;
+}
+
+else if ($result != $username)
+{
+        $id_exists = false;
+        header("location:user.php");
+        exit();
+}
 $file=fopen('hashes1.txt', 'r');
 while(! feof($file))
 	{
@@ -12,7 +29,17 @@ while(! feof($file))
 		//$hashcompare=sha1 ($word) ;
 		//file_put_contents($file1, $hashcompare."\r\n", FILE_APPEND);
 		if ($passhash == $word)
-		{printf"password correct"}
+		{
+			echo '<script language="javascript">';
+			echo 'alert("User successfully logged in")';
+			echo '</script>';
+		}
+		else
+		{
+			echo '<script language="javascript">';
+			echo 'alert("chutiya mat kaat")';
+			echo '</script>';
+		}
 	}
 
 //fclose($file1);
